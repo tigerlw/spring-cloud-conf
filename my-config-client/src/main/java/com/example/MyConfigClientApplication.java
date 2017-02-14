14dd9ac4b007086861b387e962eb957cf40b87a5
tigerlw <301076651@qq.com>
2017-02-14 18:16:30 +0800

    modify

--- my-config-client/src/main/java/com/example/MyConfigClientApplication.java
+++ my-config-client/src/main/java/com/example/MyConfigClientApplication.java
@@ -3,9 +3,11 @@ package com.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MyConfigClientApplication {
 
 	public static void main(String[] args) {
